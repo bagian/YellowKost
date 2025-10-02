7c5a5e99541bc9f7897624d0b7c980ee0cd22c1d
andinanu <48023542+Aprilandi@users.noreply.github.com>
2025-10-02 14:53:16 +0700

    fix: room show

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -146,7 +146,9 @@
                 $clone.find('button.delete-image').attr('data-tempid', data.tempid);
 
                 swiper.appendSlide($clone[0]);
-                callback();
+                if (callback != "") {
+                    callback();
+                }
             }
 
             function imageHandler(files) {
@@ -190,7 +192,9 @@
                     // console.log($('form').serialize());
                 }
                 swiper.removeSlide(slideIndex);
-                callback();
+                if (callback != "") {
+                    callback();
+                }
             });
 
             function updateInputFiles() {
